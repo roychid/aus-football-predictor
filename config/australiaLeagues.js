53276d039dda6d656fee9472e7a799b5f78ce0d5
roychid <chidhungwanaroy4@gmail.com>
2026-02-06 08:07:06 +0200

    Add Australia leagues configuration

---
+++ config/australiaLeagues.js
@@ -0,0 +1,13 @@
+export const AU_LEAGUES = {
+  AUS_A_LEAGUE: {
+    name: "A-League Men",
+    leagueAvgGoals: 1.45,
+    homeAdvantage: 1.10
+  },
+
+  AUS_NPL_QLD: {
+    name: "NPL Queensland",
+    leagueAvgGoals: 1.65,
+    homeAdvantage: 1.15
+  }
+};
